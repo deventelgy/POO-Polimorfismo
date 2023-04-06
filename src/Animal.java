ba664f698d1ca27ba9f8d82d190a3dd271459a54
Alin Piero Javier Cruz <alin.javier@entelgy.pe.local>
2023-04-06 10:12:39 -0500

    Uso de Contructor, método de acceso

---
+++ src/Animal.java
@@ -0,0 +1,44 @@
+public class Animal {
+    private String nombre;
+    private String tipo_alimentacion;
+    private int edad;
+
+    //Constructor con parámetro
+    public Animal(String _nombre, String _tipo_alimentacion, int _edad) {
+        this.nombre = _nombre;
+        this.tipo_alimentacion = _tipo_alimentacion;
+        this.edad = _edad;
+    }
+
+    //Constructor sin parametro
+    public Animal() {
+
+    }
+
+    //Método de acceso
+    public String getNombre(){
+        return nombre;
+    }
+    public void setNombre(String _nombre){
+        this.nombre = _nombre;
+    }
+    public String getTipo_alimentacion(){
+        return tipo_alimentacion;
+    }
+    public void setTipo_alimentacion(String _tipo_alimentacion){
+        this.tipo_alimentacion = _tipo_alimentacion;
+    }
+    public int getEdad(){
+        return edad;
+    }
+    public void setEdad(int _edad){
+        this.edad = _edad;
+    }
+    public void alimentarse(){
+        //cómo defino el método alimentarse para que se comporte de diferente manera según el objeto que le instancie?.
+    }
+    public void moverse(){
+        System.out.println("El animal se está moviendo");
+    }
+
+}
---
+++ src/Perro.java
@@ -0,0 +1,18 @@
+public class Perro extends Animal {
+    private String raza;
+
+    public Perro(String _raza, String _nombre, String _tipo_alimentacion, int _edad){
+        this.raza = _raza;
+    }
+   public String getRaza(){
+        return raza;
+   }
+
+   public void setRaza(String _raza){
+        this.raza = _raza;
+   }
+   public void mostrar(){
+        System.out.println(getNombre()+"-"+getTipo_alimentacion()+"-"+getEdad()+"-"+getRaza());
+   }
+
+}
